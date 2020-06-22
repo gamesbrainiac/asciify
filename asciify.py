0ec2fa042e72239fe6b06aade85137c31e59b4d5
Dan Gooding <dan.gooding2000@gmail.com>
2020-06-22 23:06:04 +0100

    Implement supersampling, producing braille chars

--- asciify.py
+++ asciify.py
@@ -1,7 +1,7 @@
 
 import argparse
 from PIL import Image
-from math import floor
+from math import floor, ceil
 import os
 
 parser = argparse.ArgumentParser(description='convert an image to ascii art')
@@ -45,12 +45,22 @@ if im.mode == 'RGBA':
 # aspect ratio of a character when its displayed 
 # (they aren't perfect squares like pixels)
 # height / width
-char_aspect = 2
+# char_aspect = 2
 
-# scale down to desired size (1 pixel per char)
-width = args.width
-height = int(width * original_height / original_width / char_aspect)
-downscaled = im.resize((width, height))
+# samples per pixel
+horiz_samples = 2
+vert_samples = 4
+
+# scale down to desired size
+pixels_across = args.width
+subpixel_width = original_width / (pixels_across * horiz_samples)
+pixels_down = original_height / (subpixel_width * vert_samples) # may not be an integer
+
+downscaled = im.resize(
+    (int(pixels_across * horiz_samples), 
+     int(pixels_down * vert_samples)))
+
+pixels_down = ceil(pixels_down)
 
 # convert RGB -> Luminosity
 greyscale = downscaled.convert('L')
@@ -61,22 +71,42 @@ min_lum, max_lum = greyscale.getextrema()
 
 pixels = list(greyscale.getdata())
 
-levels = ' +#'
+# levels = ' +#'
 # levels = ' .,-:+*?$#'
 # levels = ' ░▒▓█'
 
-if args.invert:
-    levels = levels[::-1]
+base_code = 0x2800
+
+# if args.invert:  # TODO: make it work
+#     levels = levels[::-1]
 
 rows = []
-for y in range(height):
-    char_row = []
-    for x in range(width):
-        luminance = pixels[x + y * width]
+for y in range(pixels_down):
+    char_row = [] 
+    for x in range(pixels_across):
         
-        i = floor((luminance - min_lum) / (max_lum + 1 - min_lum) * len(levels))
+        samples = 0
+        for dx in range(horiz_samples):
+            for dy in range(vert_samples):
+                i = (x * horiz_samples + dx + 
+                    (y * vert_samples + dy) * pixels_across * horiz_samples)
+                
+                if i < len(pixels):
+                    luminance = pixels[i]
+                    
+                    bit = round((luminance - min_lum) / (max_lum + 1 - min_lum))
+                    
+                else:
+                    # off the edge - just output black
+                    bit = 0
+
+                # 0 3
+                # 1 4
+                # 2 5
+                # 6 7
+                samples |= bit << (dx * 3 + dy if dy < 3 else dx + 6)
         
-        char = levels[i]
+        char = chr(base_code + samples)
         char_row.append(char)
     rows.append(''.join(char_row))
 
